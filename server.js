68a07ed7653d538745911cf9ecb222f9c2e05543
MgsTop13 <mgs350084@gmail.com>
2025-10-21 17:36:56 -0300

    mudanças
    dsawdaw

--- server.js
+++ server.js
@@ -14,13 +14,14 @@ const __dirname = path.dirname(__filename);
 api.use(cors());
 api.use(express.json());
 
-// Servir arquivos estáticos do frontend
+// Servir arquivos estáticos
+api.use('/public', express.static(path.join(__dirname, 'public')));
 api.use(express.static(path.join(__dirname, 'frontend/dist')));
 
 // Suas rotas da API
 adicionarRotas(api);
 
-// Rota para servir o frontend (React)
+// Rota para servir o frontend (React) - APÓS as rotas da API
 api.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'frontend/dist/index.html'));
 });
