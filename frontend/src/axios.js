9cde284d83c6438446fcaadcb3cf9caffdbbb18b
MgsTop13 <mgs350084@gmail.com>
2026-02-01 21:51:31 -0300

    FRONTEND PLS FUNCIONE

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "https://backlicao.onrender.com/"
+    baseURL: "https://backendlicoes.onrender.com/"
 });
 
 export default api;
